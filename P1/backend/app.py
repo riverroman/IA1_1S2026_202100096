045e7a3b7cbceec7e483c22c3551793cabcd54f2
river <3169233891503@ingenieria.usac.edu.gt>
2026-02-13 09:31:01 -0600

    [add] : struct project/backend

--- P1/backend/app.py
+++ P1/backend/app.py
@@ -1,1 +1,10 @@
-print("Hello Word");
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.get('/')
+def home():
+    return "<p>Welcome to the Flask backend!</p>"
+    
+if __name__ == '__main__':
+    app.run(debug=True)
